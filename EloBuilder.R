ecc4c34fab4b95e06ca70fcf078cc825267cca1a
Rohan Vahalia <r.vahalia@gmail.com>
2020-03-21 10:23:25 -0500

    Minor updates

--- EloBuilder.R
+++ EloBuilder.R
@@ -1,33 +1,32 @@
-  library(ggplot2)
-  library(tidyr)
-  
-  CURR_ELOS_FILE = "weekly_elo.csv"
-  
-  current_elos = read.csv(CURR_ELOS_FILE)
-  
-  d = dim(current_elos)
-  week = d[2] - 2
-  
-  names(current_elos)[names(current_elos) == "X"] <- "Players"
-  
-  names_string_curr = c('Rohan', 'John', 'Ben', 'Guillem', 'Alex', 'Neil', 'Ravi', 'Alison', 'Chris', 'Sahil')
-  
-  current_elos$Players = as.factor(current_elos$Players)
-  
-  week_levs = c()
-  week_factor = c()
-  curr_week_fac = c()
-  curr_week_levs = c()
-  for (i in (1:(week + 1))){
-    week_factor = c(week_factor, rep(i, d[1]))
-    week_levs = c(week_levs, paste('week_', i - 1, sep=''))
-  }
-  
-  week_factor = factor(week_factor)
-  levels(week_factor) = week_levs
-  
-  Long_Elos = current_elos %>% gather(week, elo, -c(Players))
-  Long_Elos$week = week_factor
-  
-  ggplot(Long_Elos, aes(x=week, y=elo, group=Players, color=Players)) + geom_line()
-  
+library(ggplot2)
+library(tidyr)
+
+CURR_ELOS_FILE = "weekly_elo.csv"
+
+current_elos = read.csv(CURR_ELOS_FILE)
+
+d = dim(current_elos)
+week = d[2] - 2
+
+names(current_elos)[names(current_elos) == "X"] <- "Players"
+
+
+current_elos$Players = as.factor(current_elos$Players)
+
+week_levs = c()
+week_factor = c()
+curr_week_fac = c()
+curr_week_levs = c()
+for (i in (1:(week + 1))){
+  week_factor = c(week_factor, rep(i, d[1]))
+  week_levs = c(week_levs, paste('week_', i - 1, sep=''))
+}
+
+week_factor = factor(week_factor)
+levels(week_factor) = week_levs
+
+Long_Elos = current_elos %>% gather(week, elo, -c(Players))
+Long_Elos$week = week_factor
+
+ggplot(Long_Elos, aes(x=week, y=elo, group=Players, color=Players)) + geom_line()
+
